7158f49cf5e1965560d6de8191fe6fcfa1aee197
Jeff Sittler <mindblender@gmail.com>
2020-04-17 14:21:21 -0700

    initial codeception test

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,14 @@
+<?php 
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Home');
+    }
+}
